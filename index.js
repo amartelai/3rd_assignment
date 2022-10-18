2c65b8038365b7c4f6a20212a552a737f6a64fac
amartelai <55350885+amartelai@users.noreply.github.com>
2022-10-18 17:30:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,64 +7,56 @@ const computerPlay = () => {
         return 'paper';
     }
     else if (randomNumber === 2) {
-        return 'scissor';
+        return 'scissors';
     }
 }
-// console.log(computerPlay());
+const playerSelection = () => {
+    const playerChoose = prompt("Enter rock, paper or scissors: ").toLowerCase(); 
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        console.log("The Game is Tied")
+    if(playerChoose !== 'rock' && playerChoose !== 'scissors' && playerChoose !== 'paper'){
+        alert("You choose bad input, try again!"); 
+        return playerSelection();
     }
-    else if (playerSelection === 'rock') {
-        if (computerSelection === 'paper') {
-            // console.log('The computer wins!');
-            return 'The computer wins!'
-        }
-        else {
-            return 'You won!';
-        }
-    }
-    else if (playerSelection === 'paper') {
-        if (computerSelection === 'scissors') {
-            return 'The computer wins!';
-        }
-        else {
-            return 'You won!';
-        }
-    }
-    else if (playerSelection === 'scissors') {
-        if (computerSelection === 'rock') {
-            return 'The computer wins!';
-        }
-        else {
-            return 'You won!';
-        }
-    }
+    else return playerChoose;
 }
 
+function playRound(cPlay, pPlay){ 
+    if(cPlay === pPlay){
+        return "The game is a draw"
+    }
+    else if (cPlay === 'rock' && pPlay === 'paper' || 
+             cPlay === 'paper' && pPlay === 'scissors' ||
+             cPlay === 'scissors' && pPlay === 'rock'
+             ){ 
+                return "Player Win!"
+             }
+             else {
+                return "Computer Win!"
+             }
+}
 
 let playerWin = 0;
 let computerWin = 0;
 let gameTied = 0;
 function game() {
     for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt(`Round ${i}: Enter either Rock, Paper or Scissors`).toLowerCase();
-        const computerSelection = computerPlay();
-        const output = playRound(playerSelection, computerSelection)
-        console.log(output)
-        if (output === 'The computer wins!') {
-            // console.log(output)
-            computerWin++;
-        }
-        else if (output === 'You won!') {
+        const player = playerSelection(); 
+        const computer = computerPlay();
+        console.log(`Computer choose: ${computer}`);
+        console.log(`Player choose: ${player}`);
+        const result = playRound(computer, player); 
+        console.log(result); 
+        if(result === 'Player Win!'){
             playerWin++;
+        }
+        else if(result === 'Computer Win!'){
+            computerWin++;
         }
         else gameTied++;
     }
 }
 game();
-console.log(`Player won ${playerWin} game/s AND Computer won ${computerWin} game/s`)
+console.log(`Player won ${playerWin} game/s AND Computer won ${computerWin} game/s. We have ${gameTied} draws`)
 if (playerWin > computerWin) {
     console.log('Player win!');
 }
@@ -75,7 +67,3 @@ else {
     console.log('There is no winner!')
 }
 
-
-
-
-// console.log(playRound(playerSelection, computerSelection))
